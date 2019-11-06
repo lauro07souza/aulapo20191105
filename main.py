541cad1b91018c4991ddd137054e43dff58b867c
lauro07souza <lauro07souza@gmail.com>
2019-11-05 21:30:42 -0300

    codigopython2

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #aulapo20191105
+pessoaNome = "ciclano"
 a = 1
 b = -5
 
 soma = a + b
 if(soma > a):
-    print("soma maior que a")
+    print(pessoaNome + ", soma maior que a")
 elif(soma == a):
-     print("soma igual a a")   
+     print(pessoaNome + ", soma igual a a")   
 else:
-    print("soma menor que a")
+    print(pessoaNome + ", soma menor que a")
+
+    print("A primeira letra do nome é: " +pessoaNome[0])
+    print("A segunda letra do nome é: " +pessoaNome[1])
+    print("A ultima letra do nome é: " +pessoaNome[6])
